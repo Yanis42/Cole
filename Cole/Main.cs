d4b50efdbf1486d89b076d8255dae069e9316352
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2022-09-06 22:42:38 +0200

    created ``Main.cs`` moved code out of MainWindow.xaml.cs added comments

---
+++ Cole/Main.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace Cole {
+    public class Main {
+        MainWindow UI = Application.Current.Windows[0] as MainWindow;
+        XElement actorFile = XElement.Load("actorList.xml");
+
+        public void Init() {
+            // initialise listbox
+            var actorList = actorFile.Elements("Actor").ToList();
+
+            foreach (var actor in actorList) {
+                string name = actor.Attribute("Name").Value;
+                UI.listBox.Items.Add(name);
+            }
+        }
+
+        // logic for the search system
+        public void SearchUpdate() {
+            // load the actor data and make a list out of it
+            var actorList = actorFile.Elements("Actor").ToList();
+
+            // get the typed text, using lower case to avoid issues
+            string searchInput = UI.searchBox.Text.ToLowerInvariant();
+
+            // make a list of the search results
+            var results = new List<string>();
+
+            // clear the listbox
+            UI.listBox.Items.Clear();
+
+            // iterate through the XML to find a match
+            // if there's one add it to the results list
+            // otherwise remove it
+            foreach (var actor in actorList) {
+                string actorName = actor.Attribute("Name").Value,
+                actorID = actor.Attribute("ID").Value, actorKey = actor.Attribute("Key").Value;
+
+                bool nameOrIdHasInput = (
+                    actorName.ToLowerInvariant().Contains(searchInput) ||
+                    actorID.ToLowerInvariant().Contains(searchInput) ||
+                    actorKey.ToLowerInvariant().Contains(searchInput)
+                );
+
+                if (nameOrIdHasInput) {
+                    results.Add(actorName);
+                } else {
+                    results.Remove(actorName);
+                }
+            }
+
+            // finally, iterate through the results to add them to the listbox on screen
+            // ``results.Distinct().ToList()`` removes duplicates
+            foreach (var result in results.Distinct().ToList()) {
+                UI.listBox.Items.Add(result);
+            }
+        }
+
+        // logic for selecting an actor from the list
+        public void ItemUpdate() {
+            var actorList = actorFile.Elements("Actor").ToList();
+
+            // get the selection's value, in this case the actor's name
+            string selectedActor = (string)UI.listBox.SelectedValue;
+
+            // iterate through the list to find the correct actor
+            foreach (var actor in actorList) {
+                string actorName = actor.Attribute("Name").Value;
+
+                // once we found it, draw the parameters
+                if (actorName == selectedActor) {
+                    Console.WriteLine("Selected Actor: {0}", actor.Attribute("ID").Value);
+                }
+            }
+        }
+    }
+}
--- Cole/MainWindow.xaml.cs
+++ Cole/MainWindow.xaml.cs
@@ -1,72 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Linq;
 
 namespace Cole {
-    /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window {
+        // prevent stack overflow exceptions
+        Main main = null;
+
+        // initialisation of the app
         public MainWindow() {
             InitializeComponent();
 
-            // initialise listbox
-            var actorList = XElement.Load("actorList.xml").Elements("Actor").ToList();
-            foreach (var actor in actorList) {
-                string name = actor.Attribute("Name").Value;
-                listBox.Items.Add(name);
-            }
+            // now that components are initialised,
+            // we can call our main class and initialise it
+            main = new Main();
+            main.Init();
         }
 
+        // update the result list on text update
         private void searchBox_TextChanged(object sender, TextChangedEventArgs e) {
-            // load the actor data and make a list out of it
-            var xml = XElement.Load("actorList.xml");
-            var actorList = xml.Elements("Actor").ToList();
+            main.SearchUpdate();
+        }
 
-            // get the typed text, using lower case to avoid issues
-            string searchInput = searchBox.Text.ToLowerInvariant();
-
-            // make a list of the search results
-            var results = new List<string>();
-
-            // clear the listbox
-            listBox.Items.Clear();
-
-            // iterate through the XML to find a match
-            // if there's one add it to the results list
-            // otherwise remove it
-            foreach (var actor in actorList) {
-                string actorName = actor.Attribute("Name").Value;
-                string actorID = actor.Attribute("ID").Value;
-                bool nameOrIdHasInput = (
-                    actorName.ToLowerInvariant().Contains(searchInput) ||
-                    actorID.ToLowerInvariant().Contains(searchInput)
-                );
-
-                if (nameOrIdHasInput) {
-                    results.Add(actorName);
-                } else {
-                    results.Remove(actorName);
-                }
-            }
-
-            // finally, iterate through the results to add them to the listbox on screen
-            // ``results.Distinct().ToList()`` removes duplicates
-            foreach (var result in results.Distinct().ToList()) {
-                listBox.Items.Add(result);
-            }
+        // show parameters on selection update
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            main.ItemUpdate();
         }
     }
 }
